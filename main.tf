ab513593728573a5bcb028925c17110cd4039355
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2024-02-23 10:13:19 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,13 +11,54 @@ provider "aws" {
   region = "eu-west-1" 
 }
 
+resource "aws_vpc" "mtc_vpc" {
+  cidr_block           = "10.123.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+  tags = {
+    Name = "dev23"
+  }
+}
+
+resource "aws_subnet" "mtc_public_subnet" {
+  vpc_id                  = aws_vpc.mtc_vpc.id
+  cidr_block              = "10.123.1.0/24"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "dev-public"
+  }
+}
+
+
+resource "aws_internet_gateway" "mtc_internet_gateway" {
+  vpc_id = aws_vpc.mtc_vpc.id
+  tags = {
+    Name = "mtc_igw"
+  }
+}
+
+resource "aws_route_table" "mtc_public_rt" {
+  vpc_id = aws_vpc.mtc_vpc.id
+  tags = {
+    Name = "dev_public_rt"
+  }
+}
+resource "aws_route" "default_route" {
+  route_table_id         = aws_route_table.mtc_public_rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.mtc_internet_gateway.id
+}
+
+resource "aws_route_table_association" "mtc_public_assoc" {
+  subnet_id      = aws_subnet.mtc_public_subnet.id
+  route_table_id = aws_route_table.mtc_public_rt.id
+}
+
 resource "aws_security_group" "spacelift_sg" {
   name        = "spacelift-sg"
   description = "Allows communication with Spacelift using fixed IPs"
-
-  vpc_id = data.aws_vpc.default.id
-
-  ingress {
+  vpc_id      = aws_vpc.mtc_vpc.id
+ ingress {
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
@@ -33,3 +74,4 @@ resource "aws_security_group" "spacelift_sg" {
     ]
   }
 }
+
