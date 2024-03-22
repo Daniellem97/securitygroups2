2ec5391cf69341d5b6abc8ce16902c1f6941a361
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2024-03-22 09:55:09 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,13 +8,10 @@ terraform {
 }
 
 provider "aws" {
-  region = "eu-west-1" 
-}
-
-provider "aws" {
   assume_role_with_web_identity {
     role_arn = var.aws_role_arn
     web_identity_token_file = "/mnt/workspace/spacelift.oidc"
+    region = "eu-west-1" 
   }
 }
 
