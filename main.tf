3e807df71a391bc0a6202d9cc1ab4e133d07d859
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2024-03-22 10:10:15 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,81 +14,11 @@ provider "aws" {
   }
 }
 
-resource "aws_vpc" "mtc_vpc" {
-  cidr_block           = "10.123.0.0/16"
-  enable_dns_hostnames = true
-  enable_dns_support   = true
+resource "aws_s3_bucket" "asfdgfds" {
+  bucket = "asfdgfds"
+
   tags = {
-    Name = "dev23"
+    Name        = "My bucket"
+    Environment = "Dev"
   }
 }
-
-resource "aws_subnet" "mtc_public_subnet" {
-  vpc_id                  = aws_vpc.mtc_vpc.id
-  cidr_block              = "10.123.1.0/24"
-  map_public_ip_on_launch = true
-  tags = {
-    Name = "dev-public"
-  }
-}
-
-
-resource "aws_internet_gateway" "mtc_internet_gateway" {
-  vpc_id = aws_vpc.mtc_vpc.id
-  tags = {
-    Name = "mtc_igw"
-  }
-}
-
-resource "aws_route_table" "mtc_public_rt" {
-  vpc_id = aws_vpc.mtc_vpc.id
-  tags = {
-    Name = "dev_public_rt"
-  }
-}
-resource "aws_route" "default_route" {
-  route_table_id         = aws_route_table.mtc_public_rt.id
-  destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = aws_internet_gateway.mtc_internet_gateway.id
-}
-
-resource "aws_route_table_association" "mtc_public_assoc" {
-  subnet_id      = aws_subnet.mtc_public_subnet.id
-  route_table_id = aws_route_table.mtc_public_rt.id
-}
-
-resource "aws_security_group" "spacelift_sg" {
-  name        = "spacelift-sg"
-  description = "Allows communication with Spacelift using fixed IPs"
-  vpc_id      = aws_vpc.mtc_vpc.id
-  
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["34.246.213.75/32"]
-  }
-  
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["52.49.218.181/32"]
-  }
-  
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["3.248.70.24/32"]
-  }
-  egress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = [
-      "0.0.0.0/0"
-    ]
-  }
-}
-
